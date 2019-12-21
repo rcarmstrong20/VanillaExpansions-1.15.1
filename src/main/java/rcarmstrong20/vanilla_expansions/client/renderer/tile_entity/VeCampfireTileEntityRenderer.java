be7f8f8607619102b83d4bd3ab2750f54b7a7e8d
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-12-21 15:39:20 -0700

    Copied the code from the 1.15.1 stonecutter

--- src/main/java/rcarmstrong20/vanilla_expansions/client/renderer/tile_entity/VeCampfireTileEntityRenderer.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/client/renderer/tile_entity/VeCampfireTileEntityRenderer.java
@@ -1,43 +1,50 @@
 package rcarmstrong20.vanilla_expansions.client.renderer.tile_entity;
 
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.matrix.MatrixStack;
 
+import net.minecraft.block.CampfireBlock;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.tileentity.CampfireTileEntityRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.CampfireTileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import rcarmstrong20.vanilla_expansions.block.VeCampfireBlock;
-import rcarmstrong20.vanilla_expansions.tile_entity.VeCampfireTileEntity;
-/*
+
 @OnlyIn(Dist.CLIENT)
-public class VeCampfireTileEntityRenderer extends TileEntityRenderer<VeCampfireTileEntity>
+public class VeCampfireTileEntityRenderer extends CampfireTileEntityRenderer
 {
-	@SuppressWarnings("deprecation")
-	public void render(VeCampfireTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage)
+	public VeCampfireTileEntityRenderer(TileEntityRendererDispatcher p_i226006_1_)
 	{
-		Direction direction = tileEntityIn.getBlockState().get(VeCampfireBlock.FACING);
-		NonNullList<ItemStack> nonnulllist = tileEntityIn.getInventory();
-		
-		for(int i = 0; i < nonnulllist.size(); ++i)
-		{
-			ItemStack itemstack = nonnulllist.get(i);
-			if (itemstack != ItemStack.EMPTY)
-			{
-				GlStateManager.pushMatrix();
-				GlStateManager.translatef((float)x + 0.5F, (float)y + 0.44921875F, (float)z + 0.5F);
-				Direction direction1 = Direction.byHorizontalIndex((i + direction.getHorizontalIndex()) % 4);
-				GlStateManager.rotatef(-direction1.getHorizontalAngle(), 0.0F, 1.0F, 0.0F);
-				GlStateManager.rotatef(90.0F, 1.0F, 0.0F, 0.0F);
-				GlStateManager.translatef(-0.3125F, -0.3125F, 0.0F);
-				GlStateManager.scalef(0.375F, 0.375F, 0.375F);
-				Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED);
-				GlStateManager.popMatrix();
-			}
-		}
+		super(p_i226006_1_);
 	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void func_225616_a_(CampfireTileEntity p_225616_1_, float p_225616_2_, MatrixStack p_225616_3_, IRenderTypeBuffer p_225616_4_, int p_225616_5_, int p_225616_6_) {
+		Direction direction = p_225616_1_.getBlockState().get(CampfireBlock.FACING);
+	      NonNullList<ItemStack> nonnulllist = p_225616_1_.getInventory();
+
+	      for(int i = 0; i < nonnulllist.size(); ++i) {
+	         ItemStack itemstack = nonnulllist.get(i);
+	         if (itemstack != ItemStack.EMPTY) {
+	            p_225616_3_.func_227860_a_();
+	            p_225616_3_.func_227861_a_(0.5D, 0.44921875D, 0.5D);
+	            Direction direction1 = Direction.byHorizontalIndex((i + direction.getHorizontalIndex()) % 4);
+	            float f = -direction1.getHorizontalAngle();
+	            p_225616_3_.func_227863_a_(Vector3f.field_229181_d_.func_229187_a_(f));
+	            p_225616_3_.func_227863_a_(Vector3f.field_229179_b_.func_229187_a_(90.0F));
+	            p_225616_3_.func_227861_a_(-0.3125D, -0.3125D, 0.0D);
+	            p_225616_3_.func_227862_a_(0.375F, 0.375F, 0.375F);
+	            Minecraft.getInstance().getItemRenderer().func_229110_a_(itemstack, ItemCameraTransforms.TransformType.FIXED, p_225616_5_, p_225616_6_, p_225616_3_, p_225616_4_);
+	            p_225616_3_.func_227865_b_();
+	         }
+	      }
+
+	   }
 }
-*/
