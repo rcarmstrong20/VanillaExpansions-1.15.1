4182dd4dc4b8149424668df5d2d308b42aa93949
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-05-01 13:00:35 -0600

    Added an item tag for the new frame blocks

---
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeItemTags.java
@@ -0,0 +1,12 @@
+package rcarmstrong20.vanilla_expansions.core;
+
+import net.minecraft.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+import rcarmstrong20.vanilla_expansions.VanillaExpansions;
+
+public class VeItemTags
+{
+	public static final Tag<Item> FRAMES = new ItemTags.Wrapper(new ResourceLocation(VanillaExpansions.MOD_ID, "frames"));
+}
