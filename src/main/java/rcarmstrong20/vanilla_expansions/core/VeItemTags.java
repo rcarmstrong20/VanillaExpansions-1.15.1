801e87d0dc8799041d344221cfb470e28727644f
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-05-12 22:50:18 -0600

    Added the paintings tag

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeItemTags.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeItemTags.java
@@ -8,5 +8,6 @@ import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
 public class VeItemTags
 {
+	public static final Tag<Item> PAINTINGS = new ItemTags.Wrapper(new ResourceLocation(VanillaExpansions.MOD_ID, "paintings"));
 	public static final Tag<Item> FRAMES = new ItemTags.Wrapper(new ResourceLocation(VanillaExpansions.MOD_ID, "frames"));
 }
