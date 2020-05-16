fb0a1f0bc240d585b7e9e6c47ff90fe97e16a596
rcarmstrong20 <rcarmstrong20@gmail.com>
2020-05-16 10:52:46 -0600

    Used suppliers in the effect methods

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeFoods.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeFoods.java
@@ -9,12 +9,10 @@ import net.minecraft.potion.Effects;
  */
 public class VeFoods
 {
-	//The supplier method doesn't work when exporting
-	@SuppressWarnings("deprecation")
-	public static final Food VOID_BUCKET = new Food.Builder().setAlwaysEdible().effect(new EffectInstance(Effects.BLINDNESS, 600, 2, true, true), 1.0F)
-																			   .effect(new EffectInstance(Effects.NIGHT_VISION, 600, 2, true, true), 1.0F)
-																			   .effect(new EffectInstance(Effects.WITHER, 200, 0, true, true), 1.0F)
-																			   .effect(new EffectInstance(Effects.WEAKNESS, 600, 2, true, true), 1.0F).build();
+	public static final Food VOID_BUCKET = new Food.Builder().setAlwaysEdible().effect(() -> new EffectInstance(Effects.BLINDNESS, 600, 2, true, true), 1.0F)
+																			   .effect(() -> new EffectInstance(Effects.NIGHT_VISION, 600, 2, true, true), 1.0F)
+																			   .effect(() -> new EffectInstance(Effects.WITHER, 200, 0, true, true), 1.0F)
+																			   .effect(() -> new EffectInstance(Effects.WEAKNESS, 600, 2, true, true), 1.0F).build();
 	
 	public static final Food GARLIC = new Food.Builder().hunger(4).saturation(4.3F).build();
 	public static final Food GREEN_ONION = new Food.Builder().hunger(2).saturation(2.2F).build();
@@ -28,10 +26,8 @@ public class VeFoods
 	public static final Food WITCHS_CRADLE_BRANCH = new Food.Builder().hunger(4).saturation(0.6F).build();
 	public static final Food WITCHS_CRADLE_SOUP = new Food.Builder().hunger(7).saturation(0.6F).build();
 	public static final Food BLOOD_VIAL = new Food.Builder().hunger(3).saturation(0.6F).build();
-	public static final Food BREAD_CRUMBS = new Food.Builder().hunger(2).saturation(3F).build();
 	public static final Food CRANBERRIES = new Food.Builder().hunger(2).saturation(0.5F).build();
 	public static final Food CRANBERRY_SAUCE = new Food.Builder().hunger(5).saturation(1.5F).build();
-	public static final Food PORKCHOP_BITS = new Food.Builder().hunger(5).saturation(10.5F).build();
 	public static final Food GINGER_ROOT = new Food.Builder().hunger(3).saturation(4.5F).build();
 	public static final Food CRISPY_SPIDER = new Food.Builder().hunger(3).saturation(13.5F).build();
 	public static final Food GRILLED_GRUB_ON_A_STICK = new Food.Builder().hunger(5).saturation(15.5F).build();
